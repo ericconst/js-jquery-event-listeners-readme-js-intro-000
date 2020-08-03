3ac5dd379e6bed78d0e1ca6612f499614781d300
Eric C <erix2205@gmail.com>
2020-08-03 11:13:08 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,9 +1,13 @@
 function getIn(){
   $('p').on('click', function(){
     alert('Hey!')
-})
 }
+)};
 
+function frameIt(){
+  $('')
+}
+function
 $(document).ready(function(){
 
 // call functions here
